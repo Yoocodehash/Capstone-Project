5e75df26f0ed5faea361cf049853d817032e0474
thepowerofone123 <73250227+thepowerofone123@users.noreply.github.com>
2023-01-22 10:29:55 -0500

    assest
    Assets that I create.

--- Project_Fox_Man_Go/Assets/Scenes/Input_Manager.cs
+++ Project_Fox_Man_Go/Assets/Scenes/Input_Manager.cs
@@ -6,15 +6,23 @@ public class Input_Manager : MonoBehaviour
 {
    PlayerControllers playerControls;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 movementInput;
 
-    // Update is called once per frame
-    void Update()
+
+    // Start is called before the first frame update
+    private void OnEnable()
     {
-        
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControllers();
+
+          //  playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+
+        }
+    }
+    // Update is called once per frame
+   private void OnDisable()
+    {
+       // playerControls.Disable();
     }
 }
--- Project_Fox_Man_Go/Assets/Scenes/PlayerControllers.cs
+++ Project_Fox_Man_Go/Assets/Scenes/PlayerControllers.cs
@@ -1,18 +1,38 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform capsule;
+
+    public float rotationSpeed;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
+        // rotate orientation
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // roate player object
+       // if (currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
+      //  {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+                capsule.forward = Vector3.Slerp(capsule.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+       }
 }
