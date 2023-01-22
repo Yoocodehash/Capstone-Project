5ec3660da1cb996191cfb923f3131827824cd47b
thepowerofone123 <73250227+thepowerofone123@users.noreply.github.com>
2023-01-21 21:02:55 -0500

    third-person
    stuck on that

---
+++ Project_Fox_Man_Go/Assets/Scenes/Input_Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Manager : MonoBehaviour
+{
+   PlayerControllers playerControls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
