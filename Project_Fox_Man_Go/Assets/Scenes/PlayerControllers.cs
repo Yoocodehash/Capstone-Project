5270fe12ad5d000740fde02c0be1ed7b1e9279c4
thepowerofone123 <73250227+thepowerofone123@users.noreply.github.com>
2023-01-22 14:37:29 -0500

    Update
    add powerup and fixed camera

--- Project_Fox_Man_Go/Assets/Scenes/PlayerControllers.cs
+++ Project_Fox_Man_Go/Assets/Scenes/PlayerControllers.cs
@@ -5,34 +5,23 @@ using UnityEngine;
 
 public class PlayerControllers : MonoBehaviour
 {
-    [Header("References")]
-    public Transform orientation;
-    public Transform player;
-    public Transform capsule;
+    public CharacterController controller;
 
-    public float rotationSpeed;
+    public float speed = 6f;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    // Update is called once per frame
     private void Update()
     {
-        // rotate orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        // roate player object
-       // if (currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
-      //  {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-            if (inputDir != Vector3.zero)
-                capsule.forward = Vector3.Slerp(capsule.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-       }
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
 }
+
