7c5ce6d39bdd2cc18dc4d746a259d68be3867472
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-01-24 16:06:42 -0500

    #3

---
+++ Project_Fox_Man_Go/Assets/Scenes/Cherry.cs
@@ -0,0 +1,21 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cherry : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+
+        if (playerInventory != null)
+        {
+            playerInventory.CherryCollected();
+            gameObject.SetActive(false);
+
+        }
+
+    }
+}
+*/
--- Project_Fox_Man_Go/Assets/Scenes/PlayerInventory.cs
+++ Project_Fox_Man_Go/Assets/Scenes/PlayerInventory.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//using UnityEngine.Events;
+
 public class PlayerInventory : MonoBehaviour
 {
-
     public int NumberOfStrawberries { get; private set; }
+
+    //public UnityEvent<PlayerInventory> OnStrawberryCollected;
 
     public void StrawberryCollected()
     {
         NumberOfStrawberries++;
+        //OnStrawberryCollected.Invoke(this);
     }
+
+
+
+    /*
+    public int NumberOfCherries { get; private set; }
+    //public UnityEvent<PlayerInventory> OnCherryCollected;
+
+    public void CherryCollected()
+    {
+        NumberOfCherries;
+        //OnCherryCollected.Invoke(this);
+    }
+    */
+
+
 
 }
