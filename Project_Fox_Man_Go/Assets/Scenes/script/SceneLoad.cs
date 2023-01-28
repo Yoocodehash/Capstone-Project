f415693df632aa45b9170648861e503f71f8a974
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-01-28 17:03:50 -0500

    #1

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/SceneLoad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+
+    public string sceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
