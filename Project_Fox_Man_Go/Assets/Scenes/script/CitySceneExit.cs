b32c98131df63dc026b975939fe84f7655e33d5e
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-01-31 16:35:46 -0500

    #1

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/CitySceneExit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CitySceneExit : MonoBehaviour
+{
+    public string sceneToLoad;
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
