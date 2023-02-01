c82072ab69e98f09428a76db447e95d07257d88b
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-02-01 16:05:17 -0500

    #3

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/CollectItems.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItems : MonoBehaviour
+{
+
+    public int strawberries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider Strawberry)
+    {
+        if(Strawberry.gameObject.tag == "Strawberry")
+        {
+            //Debug.Log("Strawberry Collected");
+            //strawberries = strawberries + 1;
+            //Strawberry.gameObject.SetActive(false);
+            //Destroy(Strawberry.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
