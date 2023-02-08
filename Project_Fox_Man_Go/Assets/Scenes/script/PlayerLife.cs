21313d929c8fa8c400d8680c4cf92911131f67ec
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-02-08 09:42:23 -0500

    #2

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/PlayerLife.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<PlayerMovement>().enabled = false;
+        ReloadLevel();
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
