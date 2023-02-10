15d83ff371671a8fde6b6b91f04967e97533fd2c
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-02-10 13:04:18 -0500

    #6

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/Health and Damage/Damage.cs
@@ -0,0 +1,46 @@
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    public playerHealth pHealth;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            pHealth.health -= damage;
+        }
+
+        
+        //if(other.gameObject.CompareTag("Player"))
+        //{
+
+            //other.gameObject.GetComponent<playerHealth>().health -= damage;
+        //}
+
+
+    }
+
+}
+
+*/
