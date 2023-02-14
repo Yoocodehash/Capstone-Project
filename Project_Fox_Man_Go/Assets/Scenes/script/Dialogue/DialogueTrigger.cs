ac30c365df0b66437e58ef36f5ef6f3cd34134ff
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-02-14 11:17:02 -0500

    #5

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public void DialogueTrigger()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
+
+*/
