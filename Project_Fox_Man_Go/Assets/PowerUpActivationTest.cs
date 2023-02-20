81e3226fcfc31ef6cefd2b1e37aab77f09aa9cee
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-02-20 12:38:55 -0500

    #3

---
+++ Project_Fox_Man_Go/Assets/PowerUpActivationTest.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpActivationTest : MonoBehaviour
+{
+
+    private float activationTime;
+
+
+    private Color col;
+    private float invisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //invisible = false;
+
+        activationTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activationTime += Time.deltaTime;
+
+        //col.a = 1;
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Invisible")
+        {
+            invisible = true;
+            activationTime = 0;
+            //col.a = 0.2;
+        }
+    }
+
+}
