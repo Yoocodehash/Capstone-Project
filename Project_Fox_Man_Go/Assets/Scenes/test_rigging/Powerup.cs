914d4e476ed4eaf88e9dcfa45a3f1aafa712b2f4
thepowerofone123 <73250227+thepowerofone123@users.noreply.github.com>
2023-02-20 14:09:42 -0500

    test
    testing player movement in level 2

--- Project_Fox_Man_Go/Assets/Scenes/test_rigging/Powerup.cs
+++ Project_Fox_Man_Go/Assets/Scenes/test_rigging/Powerup.cs
@@ -8,14 +8,21 @@ public class Powerup : MonoBehaviour
     public IceBall ice_orb;
 
 
-
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Pickup(other);
-             fire_orb.enabled = true;
-            // ice_orb.enabled = true;
+            //able to switch powerup so player can choose carefully which powerup they pick
+            if (fire_orb.enabled == true)
+            {
+                ice_orb.enabled = false;
+            }
+
+            if (ice_orb.enabled == true)// or else if?
+            {
+                fire_orb.enabled = false;
+            }
             // comment out 1 of them for the other to play they can't play at the same time.
         }
 
@@ -26,7 +33,7 @@ public class Powerup : MonoBehaviour
             ice_orb.enabled = false;
         }
 
-        if (ice_orb.enabled == true)
+        if (ice_orb.enabled == true)// or else if?
         {
             fire_orb.enabled = false;
         }
