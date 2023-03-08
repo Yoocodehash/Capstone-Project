2b63b771228f50745cea968c2a940248aa20190c
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-08 11:26:21 -0500

    #44

---
+++ Project_Fox_Man_Go/Assets/Scenes/Final Stage/Enviroment/QuitButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the game!");
+    }
+
+}
