4dc8afd9b452ab8c026a525ba8940bb2393b072d
Ethan de Lange <73250227+delange.ethan@users.noreply.github.com>
2023-03-11 10:50:11 -0500

    test
    testing thing to see how to get player not go through grass and add collision to powerup.

--- Project_Fox_Man_Go/Assets/powerupShield.cs
+++ Project_Fox_Man_Go/Assets/powerupShield.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class powerupShield : MonoBehaviour
 {
     public shieldpower shield_orb;
-    float Duration = 10f;
+    float Duration = 999999999f;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
