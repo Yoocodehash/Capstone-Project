7760f5bdf6a661be80bda6c99878ae1325f3a050
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-13 11:37:14 -0400

    #58

--- Project_Fox_Man_Go/Assets/Scenes/new health/GameOverPlayer.cs
+++ Project_Fox_Man_Go/Assets/Scenes/new health/GameOverPlayer.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverPlayer : MonoBehaviour
 {
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,25 @@ public class GameOverPlayer : MonoBehaviour
     {
         
     }
+    */
+
+    public void Restart()
+    {
+        //SceneManager.LoadScene("New Helath");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the game!");
+    }
+
 }
+
