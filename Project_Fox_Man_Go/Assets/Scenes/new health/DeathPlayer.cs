1a181bfcac5234b33b8a9e20c7f57494d095c00f
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-13 12:48:31 -0400

    #59

---
+++ Project_Fox_Man_Go/Assets/Scenes/new health/DeathPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlayer : MonoBehaviour
+{
+
+    //public GameObject GameOverCanvas;
+
+    //GameOverCanvas.SetActive(true);
+
+
+
+
+
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+
+
+
+}
