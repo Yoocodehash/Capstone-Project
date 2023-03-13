444b18897d2161707c38edc9348d8e7c9a529dd7
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-13 17:50:19 -0400

    #61

--- Project_Fox_Man_Go/Assets/Scenes/new health/DeathPlayer.cs
+++ Project_Fox_Man_Go/Assets/Scenes/new health/DeathPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathPlayer : MonoBehaviour
 {
@@ -10,7 +11,14 @@ public class DeathPlayer : MonoBehaviour
     //GameOverCanvas.SetActive(true);
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+        }
 
+    }
 
 
 
@@ -32,3 +40,144 @@ public class DeathPlayer : MonoBehaviour
 
 
 }
+
+
+
+
+
+/*
+    public float RestartSpeed = 2f;
+    bool GameIsOver = false;
+
+    public void GameOver()
+    {
+
+        if (GameIsOver == false)
+        {
+            GameIsOver = true;
+            Debug.Log("GameOver");
+            Invoke("Restart", RestartSpeed);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+ */
+
+
+
+
+/*
+     public static bool isGameOver;
+    public GameObject gameoverScreen;
+
+    void Start()
+    {
+        isGameOver = false;
+        gameoverScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+
+        if (isGameOver)
+        {
+            gameoverScreen.SetActive(true);
+        }
+
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+ */
+
+
+
+/*
+     private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("New Helath");
+        }
+    }
+ */
+
+
+
+
+/*
+    [SerializeField] private GameObject restartUI;
+    [SerializeField] private string ReloadLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelRestart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LevelRestart()
+    {
+        restartUI.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(ReloadLevel);
+    }
+*/
+
+
+
+
+
+
+/*
+
+    [SerializeField] AudioSource deathSound;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+            //Debug.Log("You have died");
+            Die();
+        }
+
+    }
+
+    
+    void Die()
+    {
+        //GetComponent<MeshRenderer>().enabled = false;
+        //GetComponent<Rigidbody>().isKinematic = true;
+        //GetComponent<PlayerMovement>().enabled = false;
+        //ReloadLevel();
+        Invoke(nameof(ReloadLevel), 1.3f);
+
+        deathSound.Play();
+
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+
+*/
