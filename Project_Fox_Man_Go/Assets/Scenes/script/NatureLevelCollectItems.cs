6f6f5a275469a4d560e2d32fa8c79b672fe4f285
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-19 11:07:44 -0400

    #75

---
+++ Project_Fox_Man_Go/Assets/Scenes/script/NatureLevelCollectItems.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NatureLevelCollectItems : MonoBehaviour
+{
+
+    public int collectables;
+
+    [SerializeField] AudioSource collectSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider Collectable)
+    {
+        if (Collectable.gameObject.tag == "Collectable")
+        {
+            Debug.Log("Collectable Collected");
+            collectables = collectables + 1;
+            Collectable.gameObject.SetActive(false);
+
+            //new
+            CollactableControl.collectableCount += 1;
+
+            collectSound.Play();
+
+            //win condition
+            if (collectables == 207)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
