7f11230ef77b3525eb687e930aee3abfeb377504
Yoocodehash <lukaszkucharczyk263@gmail.com>
2023-03-19 14:58:15 -0400

    #78

--- Project_Fox_Man_Go/Assets/Scenes/script/CollectItems.cs
+++ Project_Fox_Man_Go/Assets/Scenes/script/CollectItems.cs
@@ -8,6 +8,83 @@ public class CollectItems : MonoBehaviour
 {
 
     public int collectables;
+
+    [SerializeField] AudioSource collectSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider Collectable)
+    {
+        if(Collectable.gameObject.tag == "Collectable")
+        {
+            Debug.Log("Collectable Collected");
+            collectables = collectables + 1;
+            Collectable.gameObject.SetActive(false);
+
+            //new
+            CollactableControl.collectableCount += 1;
+
+            collectSound.Play();
+
+            //win condition
+            if(collectables == 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //endPanel.setActive(true);
+
+
+    //endPanel.setActive(false);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+//using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectItems : MonoBehaviour
+{
+
+    public int collectables;
     //public int cherries;
     //public int strawberries;
 
@@ -53,42 +130,12 @@ public class CollectItems : MonoBehaviour
 
     }
 
-    /*
-    public void OnTriggerEnter(Collider Cherry)
-    {
-        if (Cherry.gameObject.tag == "Cherry")
-        {
-            Debug.Log("Cherry Collected");
-            cherries = cherries + 1;
-            Cherry.gameObject.SetActive(false);
-            //Destroy(Cherry.gameObject);
-        }
-    }
-    */
-
-
-    /*
-        public void OnTriggerEnter(Collider Strawberry)
-    {
-        if(Strawberry.gameObject.tag == "Strawberry")
-        {
-            Debug.Log("Strawberry Collected");
-            strawberries = collectables + 1;
-            Strawberry.gameObject.SetActive(false);
-            //Destroy(Strawberry.gameObject);
-
-            collectSound.Play();
-
-        }
-
-    }
-    */
 
 
 
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
 {
 
 }
@@ -97,21 +144,7 @@ public class CollectItems : MonoBehaviour
 
 
 
-    
-     /*
-    void SetCountText()
-    {
-        countText.text = "Count: " + countText.ToString();
-        //countText.text = "Count: " + count.ToString();
-        if (count >= 2)
-            //if (count >= 12)
-            {
-            //winTextObject.SetActive(true);
-            menuManager.WinGame();
-            }
-
-     }
-     */
+   
 
      
 
@@ -122,3 +155,64 @@ public class CollectItems : MonoBehaviour
 
 
 }
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+/*
+public void OnTriggerEnter(Collider Cherry)
+{
+    if (Cherry.gameObject.tag == "Cherry")
+    {
+        Debug.Log("Cherry Collected");
+        cherries = cherries + 1;
+        Cherry.gameObject.SetActive(false);
+        //Destroy(Cherry.gameObject);
+    }
+}
+*/
+
+
+
+/*
+public void OnTriggerEnter(Collider Strawberry)
+{
+if(Strawberry.gameObject.tag == "Strawberry")
+{
+    Debug.Log("Strawberry Collected");
+    strawberries = collectables + 1;
+    Strawberry.gameObject.SetActive(false);
+    //Destroy(Strawberry.gameObject);
+
+    collectSound.Play();
+
+}
+
+}
+*/
+
+
+
+/*
+void SetCountText()
+{
+countText.text = "Count: " + countText.ToString();
+//countText.text = "Count: " + count.ToString();
+if (count >= 2)
+   //if (count >= 12)
+   {
+   //winTextObject.SetActive(true);
+   menuManager.WinGame();
+   }
+
+}
+*/
---
+++ Project_Fox_Man_Go/Assets/Scenes/script/WinMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+    public void ReplayLevel()
+    {
+        //SceneManager.LoadScene("New Helath");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("level-2");
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the game!");
+    }
+
+
+
+
+
+
+
+}
