6824ffa6d8d1285c2a1512d87524e1927c476e7a
Ethan de Lange <73250227+delange.ethan@users.noreply.github.com>
2023-03-20 14:15:38 -0400

    win screen
    when collect item win screen will show.

--- Project_Fox_Man_Go/Assets/Scenes/script/CollectItems.cs
+++ Project_Fox_Man_Go/Assets/Scenes/script/CollectItems.cs
@@ -10,8 +10,8 @@ public class CollectItems : MonoBehaviour
 
     [SerializeField] AudioSource collectSound;
 
-    public WinMenu winMenu;
-
+    // public WinMenu winMenu;
+    public GameObject winMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,7 +35,8 @@ public class CollectItems : MonoBehaviour
             if(collectables == 2)
             {
                 //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-                winMenu.WinGame();
+                //winMenu.WinGame();
+                winMenu.SetActive(true);
             }
 
         }
